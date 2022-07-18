27cbb28895c24080eae4a799a9f9e3d85a876c5e
soohoonlee <jjinimania@gmail.com>
2022-07-18 15:41:26 +0900

    Create a Dynamic Module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from '../coffees/coffees.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    }),
+    CoffeesModule,
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
